2f01c7decf2d0227836ac03e293980f8bd3f184c
climaxhome <57805640+climaxhome@users.noreply.github.com>
2020-03-24 01:11:41 +0700

    Person, state, type, interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './components/Person/Person';
 
 function App() {
+  const state = {
+    persons: [
+      {name : 'Praew', age : 19},
+      {name : 'Home', age : 20},
+      {name : 'Piglet', age : 15}
+    ]
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>เกม </h1>
+      <Person name={state.persons[0].name} age='19'>My sister is so noisy</Person>
     </div>
+
   );
 }
 
---
+++ src/components/Person/Person.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+type personProps = {
+    name: string;
+    age: string;
+    children: string;
+};
+
+// interface personProps {
+//     name: string;
+//     age: string;
+//     children?: string;
+// };
+
+const person = (props : personProps) => {
+    return(
+        <div>
+            <p>I'm {props.name} and I'm {props.age}</p>
+            <p>{props.children}</p>
+        </div>
+    );
+};
+
+export default person;
